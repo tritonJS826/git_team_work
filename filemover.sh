76b325614724c34c2db86d67b50278edd46e9cc5
OxanaTs <kseniats08@gmail.com>
2024-04-02 19:48:28 +0200

    issue3 resolved

--- filemover.sh
+++ filemover.sh
@@ -7,7 +7,19 @@
 # Проверка существования исходной директории
 
 # Проверка существования целевой директории
+targetdirectory=$(/target_directory)
+sourcedirectory=$(/source_directory)
 
+if ! test -d "$targetdirectory" || "$sourcedirectory" 
+then
+echo "directories are not found"
+exit 1
+
+if ! test -r "$targetdirectory" || "$sourcedirectory"
+then
+echo "directories are not are not available"
+exit 1 
+fi
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
