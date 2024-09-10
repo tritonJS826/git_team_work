8bd23974da04f90be99ca9c78d6f4710dc432d12
Anastasiia Yanitska <martynovaanastasiia21@gmail.com>
2024-09-10 11:19:11 +0200

    task 5

--- 2/copyFiles.sh
+++ 2/copyFiles.sh
@@ -2,3 +2,12 @@
 
 read -p "Insert search directory: " dir_search
 read -p "Insert target directory: " dir_target
+
+
+
+
+for file in $matching_files; do
+    cp "$file" "$target_directory"
+    echo "File copynig: $file"
+done
+    echo "Done."
