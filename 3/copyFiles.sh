b64fc4bf506d6489d6a826755ac67ea5e4772d72
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-09-10 09:58:33 +0200

    ADD: ISSUE #5. DEL: other files.

--- 3/copyFiles.sh
+++ 3/copyFiles.sh
@@ -9,3 +9,14 @@ if [ -z "$files" ]; then
     echo "Ошибка: Нет файлов с расширением .$extension в исходной директории!"
     exit 1
 fi
+
+
+# Copy files with the specified extension to the target directory #5
+# Копирование файлов с указанным расширением в целевой каталог #5
+# $files - полученные
+for file in $files; do
+    cp "$file" "$target_dir"
+    echo "Файл $file скопирован в $target_dir"
+done
+
+echo "Копирование завершено!"
